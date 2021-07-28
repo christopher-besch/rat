53f5c9ce2aa03eb229fc42813aef56526ccccc56
Adam Jędrzejewski <adamjedrzejewski@icloud.com>
2021-07-28 20:50:34 +0200

    check if files exist

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::path::Path;
+
+
+// TODO: handle -u option
 fn main() {
-    println!("Hello, world!");
+    let no_buffering_option: String = String::from("-u");
+    let args: Vec<String> = env::args().collect::<Vec<String>>()[1..].to_vec();
+    let files: Vec<&String> = args.iter().filter(|arg| *arg != &no_buffering_option).collect();
+    let _ = args.contains(&no_buffering_option);
+    for file in files {
+        if !Path::new(file).exists() {
+            println!("rat: {}: No such file or directory", file);
+            continue;
+        }
+    }
 }
