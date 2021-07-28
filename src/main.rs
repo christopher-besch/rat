aed27516ac67d35651f13f0b99aa760a568affb2
Adam Jędrzejewski <adamjedrzejewski@icloud.com>
2021-07-28 21:35:29 +0200

    basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::env;
 use std::path::Path;
+use std::fs;
+use std::io::{self, Write};
 
+
+fn print_file_contents(filename: &String) {
+    if let Ok(data) = fs::read(filename) {
+        io::stdout().write_all(&data);
+    }
+}
 
 // TODO: handle -u option
 fn main() {
@@ -8,10 +16,19 @@ fn main() {
     let args: Vec<String> = env::args().collect::<Vec<String>>()[1..].to_vec();
     let files: Vec<&String> = args.iter().filter(|arg| *arg != &no_buffering_option).collect();
     let _ = args.contains(&no_buffering_option);
+    
     for file in files {
-        if !Path::new(file).exists() {
-            println!("rat: {}: No such file or directory", file);
+        let path = Path::new(file);
+
+        if !path.exists() {
+            eprintln!("rat: {}: No such file or directory", file);
             continue;
         }
+        if path.is_dir() {
+            eprintln!("rat: {}: Is a directory", file);
+            continue;
+        }
+
+        print_file_contents(file);
     }
 }
