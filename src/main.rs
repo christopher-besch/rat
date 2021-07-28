f2b3542d643974a57a5de1dfbc8f693bd165d3a3
Adam Jędrzejewski <adamjedrzejewski@icloud.com>
2021-07-28 21:39:34 +0200

    removed -u option code

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,16 @@ use std::io::{self, Write};
 fn print_file_contents(filename: &String) {
     if let Ok(data) = fs::read(filename) {
         io::stdout().write_all(&data);
+    } else {
+
     }
 }
 
-// TODO: handle -u option
 fn main() {
     let no_buffering_option: String = String::from("-u");
     let args: Vec<String> = env::args().collect::<Vec<String>>()[1..].to_vec();
+    // remove -u option
     let files: Vec<&String> = args.iter().filter(|arg| *arg != &no_buffering_option).collect();
-    let _ = args.contains(&no_buffering_option);
     
     for file in files {
         let path = Path::new(file);
